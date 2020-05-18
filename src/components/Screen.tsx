bee1a2e9a8130e5224119d8e1e70c494eff829de
Rich Wareham <rich@richwareham.com>
2020-05-18 22:34:22 +0100

    reqork redraw to use canvas

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import useComponentSize from '@rehooks/component-size'
-
-import './Screen.css';
+import useComponentSize from '@rehooks/component-size';
 
 export interface Cell {
   glyph: string;
@@ -48,59 +46,90 @@ export class Buffer {
   }
 }
 
-export interface ScreenProps extends React.ComponentProps<'div'> {
+export interface ScreenProps extends React.ComponentProps<'canvas'> {
   buffer: Buffer;
   onResize?: (event: {lineCount: number, columnCount: number}) => void;
 }
 
-export default ({buffer, onResize, ...elementProps}: ScreenProps) => {
-  const font = '16px "PxPlus IBM VGA8"';
-  const glyphWidth = 8, glyphHeight = 16;
+interface FontSpec {
+  font: string;
+  glyphWidth: number;
+  glyphHeight: number;
+}
 
-  const ref = React.useRef<HTMLDivElement>(null);
+const useFont = () => {
+  const [fontSpec, setFontSpec] = React.useState<FontSpec>({
+    font: '16px monospace', glyphWidth: 8, glyphHeight: 16,
+  });
+
+  React.useEffect(() => {
+    new FontFace(
+      'PxPlus IBM VGA9',
+      'url("/fonts/PxPlus%20(TrueType%20-%20extended%20charset)/PxPlus_IBM_VGA9.ttf") format("truetype")'
+    ).load().then(fontFace => {
+      document.fonts.add(fontFace);
+      setFontSpec({font: `16px "${fontFace.family}"`, glyphWidth: 9, glyphHeight: 16});
+    });
+  }, [setFontSpec]);
+
+  return fontSpec;
+};
+
+export default ({buffer, onResize, ...elementProps}: ScreenProps) => {
+  const ref = React.useRef<HTMLCanvasElement>(null);
   const {width, height} = useComponentSize(ref);
+  const {font, glyphWidth, glyphHeight} = useFont();
+  const lastDrawnBufferRef = React.useRef<Buffer | null>(null);
 
   const lineCount = Math.floor(height / glyphHeight);
   const columnCount = Math.floor(width / glyphWidth);
+  const scale = window.devicePixelRatio;
+
+  const redraw = (redrawAll: boolean) => {
+    if(!ref.current) { return; }
+    const ctx = ref.current.getContext('2d', {alpha: false});
+    if(!ctx) { return; }
+
+    ctx.save();
+
+    ctx.scale(scale, scale);
+    ctx.font = font;
+
+    buffer.lines.forEach((line, lineIdx) => {
+      const lastDrawnLine = lastDrawnBufferRef.current && lastDrawnBufferRef.current.lines[lineIdx];
+      const topY = lineIdx * glyphHeight;
+      if(redrawAll || (lastDrawnLine !== line)) {
+        line.forEach((cell, cellIdx) => {
+          const lastDrawnCell = lastDrawnLine && lastDrawnLine[cellIdx];
+          if(redrawAll || (lastDrawnCell !== cell)) {
+            const leftX = cellIdx * glyphWidth;
+            ctx.fillStyle = cell.backgroundColour;
+            ctx.fillRect(leftX, topY, glyphWidth, glyphHeight);
+            ctx.fillStyle = cell.foregroundColour;
+            ctx.textBaseline = 'bottom';
+            ctx.fillText(cell.glyph, leftX, topY + glyphHeight);
+          }
+        });
+      }
+    });
+
+    ctx.restore();
+
+    lastDrawnBufferRef.current = buffer;
+  };
 
   React.useEffect(() => {
-    if(onResize) {
-      onResize({lineCount, columnCount});
-    }
+    onResize && onResize({lineCount, columnCount});
   }, [lineCount, columnCount]);
 
-  return (
-    <div ref={ref} {...elementProps}>
-    {
-      buffer.lines.map((lineCells, lineIdx) => (
-        <div
-          key={lineIdx}
-          style={{
-            font,
-            lineHeight: `${glyphHeight}px`,
-            height: `${glyphHeight}px`,
-            overflowX: 'hidden',
-            whiteSpace: 'nowrap',
-          }}
-        >
-        {
-          lineCells.map(({glyph, foregroundColour, backgroundColour}, cellIdx) => (
-            <span
-              key={cellIdx}
-              style={{
-                display: 'inline-block',
-                color: foregroundColour,
-                backgroundColor: backgroundColour,
-                whiteSpace: 'pre',
-              }}
-            >{
-              glyph
-            }</span>
-          ))
-        }
-        </div>
-      ))
-    }
-    </div>
-  );
+  React.useEffect(() => {
+    if(!ref.current) { return; }
+    ref.current.width = width * scale;
+    ref.current.height = height * scale;
+    redraw(true);
+  }, [width, height]);
+  React.useEffect(() => {console.log(font); redraw(true);}, [font]);
+  React.useEffect(() => {redraw(false);}, [buffer]);
+
+  return <canvas ref={ref} {...elementProps} />;
 };
